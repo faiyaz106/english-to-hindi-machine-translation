e6ef138cb8ea3edf3fc471cb96b2614475fd3107
faiyaz106 <faiyazahmed106@gmail.com>
2024-07-21 18:31:44 -0400

    minor debug in train.py

--- dataset.py
+++ dataset.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn 
 from torch.utils.data import Dataset
 
+
+
+
 class BilingualDataset(Dataset):
 
     def __init__(self, ds, tokenizer_src, tokenizer_tgt,src_lang, tgt_lang, seq_len) -> None:
--- train.py
+++ train.py
@@ -14,70 +14,6 @@ from tqdm import tqdm
 import warnings
 from config import get_config, get_weights_file_path, latest_weights_file_path
 
-"""
-def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
-    sos_idx = tokenizer_tgt.token_to_id('[SOS]')
-    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
-    # Precompte the encoder output and reuse it for every token we get from the decoder
-    encoder_output = model.encode(source, source_mask)
-
-    # Initialize the decoder input with the sos token 
-    decoder_input = torch.empty(1,1).fill_(sos_idx).type_as(source).to(device)
-    while True: 
-        if decoder_input.size(1) == max_len: 
-            break
-        # Build mask for the target (decoder input)
-
-        decoder_mask = causal_mask(decoder_input.size(1)).type_as(source_mask).to(device)
-
-        # Calculate the output of the decoder
-        out = model.decode(encoder_output,source_mask, decoder_input, decoder_mask)
-
-        # Get the next token
-        prob = model.project(out[:,-1])
-        # Select the token with the max probability (because it is a greedy search)
-        _, next_word = torch.max(prob, dim=1)
-        decoder_input = torch.cat([decoder_input, torch.empty(1,1).type_as(source).fill_(next_word.item()).to(device)],dim=1)
-        if next_word == eos_idx: 
-            break 
-    return decoder_input.sequeeze(0)
-
-
-def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, device, print_msg, global_step, writer, num_examples=2):
-    model.eval()
-    count=0
-    source_texts = []
-    expected = [] 
-    predicted = [] 
-
-    # Size of the control window (just use a default value)
-    console_width = 80 
-
-    with torch.no_grad():
-        for batch in validation_ds:
-            count+=1 
-            encoder_input = batch['encoder_input'].to(device) # (b, seq_len)
-            encoder_mask = batch['encoder_mask'].to(device)  # (b, 1, 1, seq_len)
-
-            assert encoder_input.size(0) == 1, "Batch size must be 1 for validation"
-
-            model_out = greedy_decode(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device)
-
-            source_text = batch['src_text'][0]
-            target_text = batch['tgt_text'][0]
-            model_out_text = tokenizer_tgt.decode(model_out.detach().cpu().numpy())
-            source_texts.append(source_text)
-            expected.append(target_text)
-            predicted.append(model_out_text )
-            # Print to the console
-            print_msg('-'*console_width)
-            print_msg(f'SOURCE:{source_text}')
-            print_msg(f'TARGET:{target_text}')
-            print_msg(f'PREDICTED:{model_out_text}')
-            if count == num_examples:
-                break
-"""
-
 def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
     sos_idx = tokenizer_tgt.token_to_id('[SOS]')
     eos_idx = tokenizer_tgt.token_to_id('[EOS]')
@@ -168,8 +104,8 @@ def get_ds(config):
         tokenizer_src = bpe_tokenizer(config, ds_raw, config['lang_src'])
         tokenizer_tgt = bpe_tokenizer(config, ds_raw, config['lang_tgt'])
     else:
-        tokenizer_src = bpe_tokenizer(config, ds_raw, config['lang_src'])
-        tokenizer_tgt = bpe_tokenizer(config, ds_raw, config['lang_tgt'])
+        tokenizer_src = bpe_tokenizer(config, ds_raw, config['lang_src'])    # Need to edit
+        tokenizer_tgt = bpe_tokenizer(config, ds_raw, config['lang_tgt'])    # NEed to edit
 
     # Keep 90% for training and 10% for validation 
     train_ds_size = int(0.9*len(ds_raw))
@@ -234,8 +170,10 @@ def train_model(config):
         print(f'Preloading model {model_filename}')
         state = torch.load(model_filename)
         initial_epoch = state['epoch'] + 1
+        model.load_state_dict(state['model_state_dict'])
         optimizer.load_state_dict(state['optimizer_state_dict'])
         global_step = state['global_step']
+
     else:
         print('No model to preload, starting from scratch')
 
@@ -282,8 +220,6 @@ def train_model(config):
             'model_state_dict' : model.state_dict(),
             'optimizer_state_dict' : optimizer.state_dict(),
             'global_step' : global_step
-
-
         }, model_filename)
 
 if __name__ == '__main__':
